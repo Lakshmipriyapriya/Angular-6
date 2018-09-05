d35b4228ccca47082301bcd2fd29ef68956fc08d
Lakshmipriyapriya <39649881+Lakshmipriyapriya@users.noreply.github.com>
2018-09-06 02:04:37 +0630

    Add files via upload

--- src/app/sample-data/sample-data.component.ts
+++ src/app/sample-data/sample-data.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SampleDataComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+   }
+   
 
   ngOnInit() {
   }
